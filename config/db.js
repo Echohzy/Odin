3d54d2aac9a513b1bca5cd6a8fa4ff8df29adde3
Echohzy <1820123540@qq.com>
2016-11-19 21:49:58 +0800

    sign in

--- config/db.js
+++ config/db.js
@@ -1,4 +1,13 @@
 var mongoose = require("mongoose");
 
+var userSchema = mongoose.Schema({
+  nick_name: String,
+  email: String,
+  password: String,
+  avatar: String 
+});
+
+module.exports.ser = mongoose.model("user", userSchema);
+
 
 
