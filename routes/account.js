63ac6121f512206e56d2cbf73fcbc55bcbc12905
ziyi <ziyi.huang@ceasia.com.cn>
2017-03-02 17:00:20 +0800

    route

--- routes/account.js
+++ routes/account.js
@@ -22,10 +22,9 @@ router.get("/:id/sign_in", function(req, res, next){
   });
 });
 
-
-/*update account*/
-router.put("/:id", function(req, res, next){
-  accountModule.update(req.params.id, req.body)
+/*add account*/
+router.post("/", function(req, res, next){
+  accountModule.addAccount(req.body)
   .then(function(data){
     res.json({
       status: "success",
@@ -39,8 +38,72 @@ router.put("/:id", function(req, res, next){
   });
 });
 
-/*sign out api*/
 
+/*update account*/
+router.put("/:id", function(req, res, next){
+  accountModule.updateAccount(req.params.id, req.body)
+  .then(function(data){
+    res.json({
+      status: "success",
+      data: data
+    });
+  }, function(error){
+    res.json({
+      status: "error",
+      message: error
+    });
+  });
+});
+
+/*delete account*/
+router.delete("/:id", function(req, res, next){
+  accountModule.deleteAccount(req.params.id)
+  .then(function(data){
+    res.json({
+      status: "success",
+      data: data
+    });
+  }, function(error){
+    res.json({
+      status: "error",
+      message: error
+    });
+  })
+});
+
+/*get account*/
+router.get("/:id", function(req, res, next){
+  accountModule.getAccount(req.params.id)
+  .then(function(data){
+    res.json({
+      status: "success",
+      data: data
+    });
+  }, function(error){
+    res.json({
+      status: "error",
+      message: error
+    });
+  });
+});
+
+/*get account list*/
+router.get("/", function(req, res, next){
+  accountModule.listAccount(req.query)
+  .then(function(data){
+    res.json({
+      status: "success",
+      data: data
+    });
+  }, function(error){
+    res.json({
+      status: "error",
+      message: error
+    });
+  })
+});
+
+/*sign out api*/
 router.delete("/:id/sign_out",  function(req, res, next){
   req.session.destroy(function(err){
     if(!err){
