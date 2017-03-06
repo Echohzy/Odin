3b1d1ac46cbca1445b01305617faab324b5797f6
ziyi <ziyi.huang@ceasia.com.cn>
2017-03-06 11:03:26 +0800

    article module

---
+++ modules/article_module.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var db = require("../config/db.js");
+var  mongoose = require("mongoose");
+
+/*app article*/
+module.exports.addArticle = function(params){
+  var article = db.article(params);
+  article.save(function(error, data){
+    if(error){
+      reject(error);
+    }else{
+      resolve(data);
+    }
+  });
+};
+
+/*get Article*/
+module.exports.getArticle = function(id){
+  return new Promise(function (resolve, reject){
+    db.article.findOneById(id, function (error, data){
+      if(error){
+        reject(error);
+      }else{
+        resolve(data);
+      }
+    });
+  });
+};
+
+/*list article*/
+module.exports.listArticle = function(params){
+  return new Promise(function (resolve, reject){
+    db.article.find(params, function (error, data){
+      if(error){
+        reject(error);
+      }else{
+        resolve(data);
+      }
+    });
+  });
+};
+
+/*update article*/
+module.exports.updateArticle = function(id, params){
+  return new Promise(function (resolve, reject){
+    db.article.findByIdAndUpdate(id, {$set: params}, function(error, data){
+      if(error){
+        reject(error);
+      }else{
+        resolve(data);
+      }
+    })
+  });
+};
+
+/*delete article*/
+module.exports.deleteArticle = function(id){
+  return new Promise(function (resolve, reject){
+    db.article.findOneAndRemove({_id: id}, function (error, data){
+      if(error){
+        reject(error);
+      }else{
+        resolve(data);
+      }
+    });
+  });
+};
