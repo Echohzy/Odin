8dde04e93b6f7b55d317241d13e0bb1f6dc5eb4b
ziyi <ziyi.huang@ceasia.com.cn>
2017-03-06 14:55:44 +0800

    some detail

---
+++ routes/article.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var express = require("express");
+var router = express.Router();
+var articleModule = require("../mosules/article_module");
+
+/*add article api*/
+router.post("/", function (req, res, next){
+  articleModule.addArticle(req.body)
+  .then(function(data){
+    res.json({
+      status: "success",
+      data: data
+    });
+  }).catch(function(error){
+    res,json({
+      status: "error",
+      message: error
+    })
+  });
+});
+
+/*update article api*/
+router.put("/:id", function (req, res, next){
+  articleModule.updateColumn(req.params.id, req.body)
+  .then(function(data){
+    res.json({
+      status: "success",
+      data: data
+    });
+  }).catch(function(error){
+    res.json({
+      status: "error",
+      message: error
+    });
+  });
+});
+
+/*get article api*/
+router.get("/:id", function (req, res, next){
+  articleModule.getArticle(req.params.id)
+  .then(function(data){
+    res.json({
+      status: "success",
+      data: data
+    });
+  }).catch(function(error){
+    res.json({
+      status: "error",
+      message: error
+    });
+  })
+});
+
+/*list article api*/
+router.get("/", function (req, res, next){
+  articleModule.listArticle(req.params.id, req.query)
+  .then(function(data){
+    res.json({
+      status: "success",
+      data: data
+    });
+  }).catch(function(error){
+    res.json({
+      status: "error",
+      message: error
+    });
+  })
+});
+
+/*delete article api*/
+router.delete("/:id", function (res, res, next){
+  articleModule.deleteArticle(req.params)
+  .then(function(data){
+    res.json({
+      status: "success",
+      data: data
+    });
+  }).catch(function(error){
+    res.json({
+      status: "error",
+      message: error
+    });
+  })
+});
