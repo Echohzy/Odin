f624504f02803ec7d2ae5ba71a725b3f683f5086
Echohzy <1820123540@qq.com>
2017-03-19 13:41:07 +0800

    path

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.use(express.static(path.join(__dirname, 'src')));
 
 app.use(webpackDevMiddleware(compiler,{
