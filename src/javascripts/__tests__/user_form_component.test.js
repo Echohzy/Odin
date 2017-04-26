ece1e6cffe896789ba7f223af0ba1009fe2cb483
ziyi <ziyi.huang@ceasia.com.cn>
2017-04-26 16:57:08 +0800

    add test

---
+++ src/javascripts/__tests__/user_form_component.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import { shallow } from 'enzyme';
+
+import React from 'react';
+
+import UserFormComponent from '../components/user_form_component.jsx';
+
+import FormInputComponent from '../components/form_input_component.jsx';
+
+describe('<UserFormContainer />', ()=>{
+  it('should render five <FormInputComponent />', () => {
+    const wrapper = shallow(<UserFormComponent />);
+    expect(wrapper.find(FormInputComponent)).toHaveLength(5);
+  });
+});
