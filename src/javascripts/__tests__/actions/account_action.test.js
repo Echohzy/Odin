696089d5f23c4f039d0ba237524afbad5e3c0108
ziyi <ziyi.huang@ceasia.com.cn>
2017-04-28 18:06:29 +0800

    test mock

--- src/javascripts/__tests__/actions/account_action.test.js
+++ src/javascripts/__tests__/actions/account_action.test.js
@@ -4,18 +4,21 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import * as actions from '../../actions/account_action.jsx';
 
+function wrapperFunction(callback){
+  callback();
+}
 const mockStore = configureMockStore([thunk]);
 
 describe('account_action', ()=>{
   let data = {name: "test"};
   it("received data", ()=>{
-    expect(actions.receivedData("test_reducer", data)).toEqual({type: actions.RECEIVED_DATA, reducerName: "test_reducer", data:data});
+    expect(actions.receivedData("test_reducer", data)).toEqual({type: actions.RECEIVED_DATA, reducerName: "tes_reducer", data:data});
   });
   it("test mock", ()=>{
-    let count = 12;
-    const mockFn = jest.fn((value)=>console.log(value)).mockImplementationOnce(scalar =>  count+=scalar).mockImplementationOnce(scalar =>  count+=scalar);
-    const a = new mockFn(1);
-    const b = new mockFn(2);
-    console.log(mockFn.mock.instances);
+    const returnsTrue = jest.fn(() => console.log(true));
+    returnsTrue();
+    const mockFn = jest.fn();
+    mockFn();
+    expect(mockFn).not.toHaveBeenCalled();
   });
 });
